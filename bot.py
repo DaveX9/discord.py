0a3e030a5af28e8c8f5f3d729c79eef0f72318cb
DaveX9 <sahadevthapa645462@gmail.com>
2026-02-17 17:08:24 +0700

    update env

--- bot.py
+++ bot.py
@@ -150,20 +150,3 @@ async def unmute(ctx, member: discord.Member):
 # ================= RUN =================
 bot.run(TOKEN)
 
-from flask import Flask
-from threading import Thread
-
-app = Flask('')
-
-@app.route('/')
-def home():
-    return "Bot is alive"
-
-def run():
-    app.run(host='0.0.0.0', port=8080)
-
-def keep_alive():
-    t = Thread(target=run)
-    t.start()
-
-keep_alive()
